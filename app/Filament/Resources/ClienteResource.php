1ed31cb1648bff9cc546e5b9459b729473d6a4da
Wellington Cavalcante <wellarts@gmail.com>
2024-10-14 10:12:30 -0300

    fone

--- app/Filament/Resources/ClienteResource.php
+++ app/Filament/Resources/ClienteResource.php
@@ -129,8 +129,7 @@ class ClienteResource extends Resource
             Tables\Columns\TextColumn::make('cidade.nome')
                 ->label('Cidade'),
             Tables\Columns\TextColumn::make('telefone_1')
-
-                ->formatStateUsing(fn (string $state) => vsprintf('(%d%d)%d%d%d%d%d-%d%d%d%d', str_split($state)))
+               // ->formatStateUsing(fn (string $state) => vsprintf('(%d%d)%d%d%d%d%d-%d%d%d%d', str_split($state)))
                 ->label('Telefone'),
 
             Tables\Columns\TextColumn::make('email')
